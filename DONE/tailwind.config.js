2a7b768d948b11c08d516a6976eeb80158d5add1
Giga <49904920+GigaJachvadze@users.noreply.github.com>
2024-10-15 14:32:47 +0400

    update

---
+++ DONE/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'background': '#1B1A3A',
+        'card-background': '#232043',
+        'exchange-background': '#3D3B58',
+        'button': '#6A5ACD',
+        'button-hover': '#836FFF',
+        'button-primary': '#00BFA6',
+        'button-primary-hover': '#009F89',
+        'main-text': '#D3D1E3',
+        'sub-text': '#9A99B2',
+        'button-primary-text': '#1A1A2E'
+      },
+      fontFamily: {
+        notoSansGeorgian: ['noto-sans-georgian', 'sans-serif']
+      },
+      fontSize: {
+        'body-desktop': '100%',
+        'body-xl': '80%',
+        'body-mobile': '75%'
+      }
+    }
+  },
+  plugins: [],
+}
+
